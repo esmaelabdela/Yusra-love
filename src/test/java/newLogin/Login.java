741ce001c1a7ea005c4d327aafa83477533b9866
esmaelabdela <146069460+esmaelabdela@users.noreply.github.com>
2024-01-18 01:02:41 -0600

    am back

--- src/test/java/newLogin/Login.java
+++ src/test/java/newLogin/Login.java
@@ -2,17 +2,18 @@ package newLogin;
 
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Before;
-import org.junit.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class Login {
 
 	public static WebDriver driver;
 	
-	@Before
+	@BeforeMethod
 	public void initDriver() {
 		System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
